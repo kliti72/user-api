45cd656acb985163fe4d91d09c7680bf6ba2fb99
gabriele tarchi <gabriele.tarchi@bcsoft.net>
2024-09-17 16:25:17 +0200

    Creati primi metodi in UserService

--- src/main/java/net/bcsoft/bcosft/controller/UserController.java
+++ src/main/java/net/bcsoft/bcosft/controller/UserController.java
@@ -1,9 +1,14 @@
 package net.bcsoft.bcosft.controller;
 
+import net.bcsoft.bcosft.dto.UsersDTO;
 import net.bcsoft.bcosft.entity.Users;
+import net.bcsoft.bcosft.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.naming.NotContextException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -12,11 +17,34 @@ import java.util.List;
 @RestController
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/users/")
-    public ResponseEntity<List <Users>>get(){
+    public ResponseEntity<List <UsersDTO>>get(){
         //la lista è uguale al metodo di selectAll
-        List <Users> userList = new ArrayList<>();
+        List <UsersDTO> userList = new ArrayList<>();
+        try{
+           userList  = userService.selectAll();
+        }catch (NotContextException e){
+            return ResponseEntity.noContent().build();
+        }
+
         return ResponseEntity.ok(userList);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<UsersDTO> getById(@PathVariable Long userId) {
+        UsersDTO usersDTO;
+
+        try {
+            usersDTO = userService.selectById(userId);
+        }catch (NotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+
+
+        return ResponseEntity.ok(usersDTO);
     }
 
     @PostMapping("/users/")
--- src/main/java/net/bcsoft/bcosft/service/UserService.java
+++ src/main/java/net/bcsoft/bcosft/service/UserService.java
@@ -1,13 +1,20 @@
 package net.bcsoft.bcosft.service;
 
+import net.bcsoft.bcosft.dto.UsersDTO;
 import net.bcsoft.bcosft.entity.Role;
 import net.bcsoft.bcosft.entity.Users;
 import net.bcsoft.bcosft.repository.UserRepository;
+import org.apache.logging.log4j.util.InternalException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.naming.NotContextException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -18,4 +25,29 @@ public class UserService {
     private UserRepository userRepository;
 
 
+    public List<UsersDTO> selectAll() throws NotContextException {
+        List <Users> usersList;
+        try{
+            usersList = userRepository.findAll();
+        }catch (RuntimeException e){
+            throw new InternalException("Errore");
+        }
+        List <UsersDTO> usersDTOList = new ArrayList<>();
+
+        if(usersList.isEmpty()) {
+            throw new NotContextException("Non presenti user");
+        }
+        for (Users users : usersList) {
+            UsersDTO usersDTO = new UsersDTO(users.getId(), users.getName(), users.getSurname(), users.getPassword(), users.getRegisterDate(), users.getLastAccess(), users.getRole().getId());
+            usersDTOList.add(usersDTO);
+        }
+        return usersDTOList;
+    }
+
+    public UsersDTO selectById(Long userId) throws NotFoundException {
+        Users users;
+        users = userRepository.findById(userId)
+                .orElseThrow(NotFoundException::new);
+        return new UsersDTO(users.getId(), users.getName(), users.getSurname(), users.getPassword(), users.getRegisterDate(), users.getLastAccess(), users.getRole().getId());
+    }
 }
