7975617d44961235a1f3f904dc645522d31ebbcc
kliti elezi <129623647+kliti72@users.noreply.github.com>
2024-09-18 12:44:45 +0200

    Role api work
    Role api workRole api workRole api work

--- src/main/java/net/bcsoft/bcosft/controller/RoleController.java
+++ src/main/java/net/bcsoft/bcosft/controller/RoleController.java
@@ -1,28 +1,71 @@
 package net.bcsoft.bcosft.controller;
 
+import jdk.jfr.Description;
 import net.bcsoft.bcosft.dto.RoleDTO;
-import net.bcsoft.bcosft.entity.Role;
-import net.bcsoft.bcosft.entity.Users;
 import net.bcsoft.bcosft.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.SQLIntegrityConstraintViolationException;
+
 
 @RestController
+
 public class RoleController {
     @Autowired
     RoleService roleService;
 
-
-    @GetMapping("/users/{userId}/role/")
+    @GetMapping("/role/user/{userId}/")
+    @Description("Recovery the role with User id")
     public ResponseEntity<RoleDTO> getByUserId(@PathVariable Long userId){
-        //la lista è uguale al metodo di selectAll
-        RoleDTO role = roleService.getRoleByUserId(userId);
+
+        RoleDTO role;
+
+        try {
+            role = roleService.getRoleByUserId(userId);
+        } catch(NotFoundException | NullPointerException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         return ResponseEntity.ok(role);
     }
+
+    @PostMapping("/role/")
+    @Description("Create new role for all user")
+    public ResponseEntity<RoleDTO> insert(@RequestBody RoleDTO roleDTO) {
+
+        RoleDTO role = roleService.insert(roleDTO);
+
+        return ResponseEntity.ok(role);
+
+    }
+
+    @PutMapping("/role/{roleId}/")
+    @Description("Update specific role")
+    public ResponseEntity<RoleDTO> put(@RequestBody RoleDTO roleDTO, @PathVariable Long roleId) {
+
+        RoleDTO role = roleService.update(roleDTO, roleId);
+
+        return ResponseEntity.ok(role);
+    }
+
+    @DeleteMapping("/role/{roleId}/")
+    @Description("Delete the role with role id")
+    public ResponseEntity<HttpStatus> delete(@PathVariable Long roleId) {
+
+        try {
+            roleService.delete(roleId);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (DataIntegrityViolationException e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
 }
