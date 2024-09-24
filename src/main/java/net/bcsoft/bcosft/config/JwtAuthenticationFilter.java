232a5a86749c6d3a23eee90a1864d531fb864bdc
kliti elezi <129623647+kliti72@users.noreply.github.com>
2024-09-24 17:56:33 +0200

    Autenticazione funzionante
    Autenticazione funzionanteAutenticazione funzionanteAutenticazione funzionante

---
+++ src/main/java/net/bcsoft/bcosft/config/JwtAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package net.bcsoft.bcosft.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import net.bcsoft.bcosft.utils.JwtTokenProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getJwtFromRequest(request);
+
+        System.out.print("FILTRO PARTITO!");
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            UserDetails userDetails = jwtTokenProvider.getUserDetailsFromToken(token);
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            System.out.println("User authenticated: " + userDetails.getUsername());
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/net/bcsoft/bcosft/controller/AuthenticationController.java
@@ -0,0 +1,66 @@
+package net.bcsoft.bcosft.controller;
+
+import net.bcsoft.bcosft.config.CustomUserDetails;
+import net.bcsoft.bcosft.dto.JwtResponseDTO;
+import net.bcsoft.bcosft.dto.UsersDTO;
+import net.bcsoft.bcosft.service.UserService;
+import net.bcsoft.bcosft.utils.JwtTokenProvider;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+    public AuthenticationController(UserService userService, JwtTokenProvider jwtTokenProvider) {
+        this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/login/")
+    public ResponseEntity<JwtResponseDTO> authenticateUser(@RequestBody UsersDTO userDTO) throws ChangeSetPersister.NotFoundException {
+        String jwt = jwtTokenProvider.generateToken(userDTO);
+        return ResponseEntity.ok(new JwtResponseDTO(jwt));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Boolean> register(@RequestBody UsersDTO user) {
+        try {
+            userService.insert(user);
+        } catch (ChangeSetPersister.NotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+        return ResponseEntity.ok(true);
+    }
+
+    @GetMapping("/validate/")
+    public ResponseEntity<Boolean> tokenToUserDTO(@RequestHeader("Authorization") String token) {
+        try {
+            jwtTokenProvider.validateToken(token);
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+        return ResponseEntity.ok(true);
+    }
+
+    @GetMapping("/current-user/")
+    public UsersDTO getCurrentUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        UsersDTO userDTO = new UsersDTO();
+
+        userDTO.setSurname(userDetails.getUsername());
+        userDTO.setEmail(userDetails.getEmail());
+        return userDTO;
+    }
+
+
+}
