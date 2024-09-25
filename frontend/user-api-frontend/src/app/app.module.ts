d770ac7317e1d05c17697ed5f231457fa3a10f5f
kliti elezi <129623647+kliti72@users.noreply.github.com>
2024-09-25 18:00:31 +0200

    Update formCapture
    Update formCaptureUpdate formCapture

--- frontend/user-api-frontend/src/app/app.module.ts
+++ frontend/user-api-frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomButtonComponent } from './components/custom-button/custom-button.component';
 
 
 @NgModule({
--- frontend/user-api-frontend/src/app/components/form-login/form-login.component.ts
+++ frontend/user-api-frontend/src/app/components/form-login/form-login.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html',
   styleUrl: './form-login.component.css',
   standalone: true,
+  imports: [ReactiveFormsModule]
 })
 export class FormLoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    // Inizializza il form con FormBuilder
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  // Metodo per gestire il submit del form di login
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+      console.log('Form data:', formData);
+      // Qui puoi fare la chiamata al backend per l'autenticazione
+    } else {
+      console.log('Form non valido');
+    }
+  }
+
+  // Metodo per login con GitHub
+  loginWithGitHub() {
+    console.log('Login con GitHub');
+    // Esegui qui il login tramite API per GitHub
+  }
+
+  // Metodo per login con Microsoft
+  loginWithMicrosoft() {
+    console.log('Login con Microsoft');
+    // Esegui qui il login tramite API per Microsoft
+  }
 }
