c382a944f6f6fae1260bb434d024095955389885
kliti elezi <129623647+kliti72@users.noreply.github.com>
2024-10-02 12:46:06 +0200

    Profilo e faq-card
    Profilo e faq-card

--- frontend/user-api-frontend/src/app/app-routing.module.ts
+++ frontend/user-api-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthenticationComponent } from './pages/authentication/authentication.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
-  {path: "login", component: AuthenticationComponent}
+  {path: "login", component: AuthenticationComponent},
+  {path: "profile", component: ProfileComponent}
 ];
 
 @NgModule({
